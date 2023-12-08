250c6ec96889161cba4f1c3751ff88162a6e0f26
Diego.C <diego.carriere@etu.umontpellier.fr>
2023-12-08 15:13:21 +0100

    init repo avec les éléments de la ndi

---
+++ flocon.js
@@ -0,0 +1,15 @@
+class Flocon {
+    private x: number;
+    private y: number;
+    private size: number;
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+
+    move(x, y, vitesse) {
+
+    }
+}
